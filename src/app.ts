459c920823fc519f20583fcfd987465bbf492a54
Juan Velasquez <velasquezjuanesteban1403@gmail.com>
2021-07-03 11:35:38 -0500

    Get Mockup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
 import express, {Application} from 'express';
+import morgan from 'morgan';
+
 
 const app: Application = express ();
+
+import routes from './routes/route'
 
 //Settings
 app.set('port',3000)
 
+//middleware
+app.use(morgan('dev'))
+
+//routes
+
+app.use(routes)
 export default app;
