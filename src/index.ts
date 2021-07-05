b8ab2f8d747ece9d6e09f2f4b9c946b029ce7176
Juan Velasquez <velasquezjuanesteban1403@gmail.com>
2021-07-05 14:53:13 -0500

    Rest V1.0

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
-import getCoachName from "./mock/getcoachnames";
 import app from "./app";
 
-var soap = require('soap');
-var url:string = 'https://ws.footballpool.dataaccess.eu/info.wso?WSDL';
-const client1= soap.createClientAsync(url).then((client:any) => {
-    return client.AllTeamCoachNames(getCoachName);
-  }).then((result:any) => {
-    console.log(result);
-  });
-/*   var url = 'https://ws.footballpool.dataaccess.eu/info.wso?WSDL';
-
-  soap.createClient(url, function(err:any, client:any) {
-      client.AllTeamCoachNames(getcoachnames, function(err:any, result:any) {
-          console.log(JSON.stringify(result));
-      });
-  }); */
 function main(){
     app.listen(app.get('port'));
 }
